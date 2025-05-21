898fa1852e4b5305783fddb43dc6c8daa1710155
Millan Ashly Type <140631750+Aly3n2077@users.noreply.github.com>
2025-05-21 12:52:47 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -196,7 +196,7 @@ function App() {
               <div className="relative">
                 <button
                   onClick={() => setIsMenuOpen(!isMenuOpen)}
-                  className="flex items-center space-x-3 hover:text-[#00ff00] dark:hover:bg-gray-700 rounded-full transition-colors"
+                  className="inline-flex items-center space-x-2 bg-[#00ff00] text-black px-4 py-2  transition-colors"
                 >
                   <span>Menu</span>
                   <ChevronRight className="w-5 h-5" />
