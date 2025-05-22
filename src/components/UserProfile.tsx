edf20a43956d67c9a3e6a13f3f00fa6cefe7c919
guidofawkes-debug <guidofawkes134@yahoo.com>
2025-05-22 11:32:21 +0200

    Updated UserProfile.tsx

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -1,8 +1,8 @@
 
 import React, { useEffect, useState } from 'react';
-import { useAuth } from '../AuthContext';
+import { useAuth } from './Auth';
 import { doc, getDoc } from 'firebase/firestore';
-import { db } from '../firebase';
+import { db } from './firebase';
 
 interface UserData {
   displayName: string;
